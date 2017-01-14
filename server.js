c7b8ced313bb496eff6d0f6ade63407b4660e41c
adventurebear <suzanneatkinson@gmail.com>
2017-01-14 02:05:37 -0500

    added process.ENV port listener

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var app = express();
 var compiler = webpack(webpackConfig);
  
 app.use(express.static(__dirname + '/www'));
- 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
@@ -18,7 +19,7 @@ app.use(webpackDevMiddleware(compiler, {
   historyApiFallback: true,
 }));
  
-var server = app.listen(3000, function() {
+var server = app.listen('port', function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
